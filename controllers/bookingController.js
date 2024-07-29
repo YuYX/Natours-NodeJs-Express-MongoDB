442dfbd1b63a480f73cb74715ea99a3b865b84a7
YuYX <yuyongxue@gmail.com>
2024-07-29 19:42:21 +0800

    Testing Tour-User-Price output3.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,8 +56,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id;
   const user = await User.findOne({email: session.customer_email}).id;
-  const price = session.line_items[0].price_data.unit_amount / 100;
- 
+  // const price = session.line_items[0].price_data.unit_amount / 100;
+  const price = session.line_items[0].amount_total / 100;
   await Booking.create({tour, user, price});
 }
 
@@ -76,7 +76,7 @@ exports.webhookCheckout = (req, res, next) => {
   }
 
   if(event.type === 'checkout.session.completed') {
-    console.log("EVENT.DATA.OBJECT:",event.data.object);
+     
     createBookingCheckout(event.data.object);
   }
   res.status(200).json({ received: true});
