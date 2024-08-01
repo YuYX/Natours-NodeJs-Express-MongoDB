cbda122eb8094137f459ebcba042e434978aabf5
YuYX <yuyongxue@gmail.com>
2024-08-01 11:57:17 +0800

    Added logging of seleced booking info.

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -54,6 +54,7 @@ exports.getLoginForm = (req, res) => {
 exports.getMyTours = catchAsync(async (req, res, next) => {
   // 1) Find all bookings
   const bookings = await Booking.find({ user: req.user.id });
+  console.log("BOOKINGS:", bookings);
 
   // 2) Find tours with the returned IDs
   const tourIDs = bookings.map(el => el.tour);   
