e92ad8805e00d4bbec2ef1982106020a6036d7e6
YuYX <yuyongxue@gmail.com>
2024-08-01 14:42:29 +0800

    Added logging of seleced booking info.

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -74,6 +74,7 @@ exports.getOne = (Model, popOptions) => catchAsync(async (req, res, next) => {
         // To allow for nested GET reviews on tour (hack)
         let filter = {};
         if(req.params.tourId) filter = { tour: req.params.tourId };
+        console.log("FILTER:", filter);
 
         const features = new APIFeatures(Model.find(filter), req.query)
           .filter()
